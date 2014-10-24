a54057edd2e377dbb35792c3eca77524a5f9d4de
Ulises Ponce Mendoza <upmendoza@gmail.com>
2014-10-24 13:52:09 -0700

    Se añaden archivos al Commit Inicial
    Parece que ya va quedando al cosa.

---
+++ src/com/example/UPM/Audioteca.java
@@ -0,0 +1,39 @@
+package com.example.UPM;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Audioteca extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        String[] vMenu = new String[] { "Reproducir", "Fonoteca", "Opciones" };
+
+        // First paramenter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          R.layout.listrow, R.id.textView1, vMenu);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                     "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                     .show();
+            }
+        });
+    }
+}
